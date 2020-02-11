495b9b1674135c9fa3fc273f6323d8db46184a54
Felipe <ninofelipe73@gmail.com>
2020-02-11 13:44:06 -0600

    checks if user is signed in and wont let them go to login page if logged in

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { userLoginFetch } from "../../redux/actions/user";
 import * as ROUTES from "../../constants/routes";
+import { Redirect } from "react-router";
 
 class Login extends Component {
   constructor(props) {
@@ -33,7 +34,12 @@ class Login extends Component {
 
   render() {
     const { error, username, password } = this.state;
+    const { currentUser } = this.props;
     const isInvalid = password === "" || username === "";
+    console.log("current", currentUser);
+    if (currentUser) {
+      return <Redirect to="/Home" />;
+    }
     return (
       <div className="registration-wrapper">
         <form onSubmit={this.handleSubmit}>
@@ -72,9 +78,13 @@ class Login extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  currentUser: state.authState.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   userLoginFetch: (username, password) =>
     dispatch(userLoginFetch(username, password))
 });
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
