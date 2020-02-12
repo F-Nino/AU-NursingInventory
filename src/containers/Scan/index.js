adc94ad25b032d918203d0693827ee928bb39c74
nengler <englernathan@gmail.com>
2020-02-12 11:21:35 -0600

    cleaned up css and fixed scan page funcionality

--- src/containers/Scan/index.js
+++ src/containers/Scan/index.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Scan extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       barcode: "",
+      checked: "add-button",
       showAdd: true,
       showDelete: false,
       showDetails: false,
-      showText: false
+      showText: false,
+      showResponse: []
     };
   }
 
-  onButtonClickHandler = () => {
-    this.setState({showMessage: true});
+  onButtonClickHandler = event => {
+    event.preventDefault();
+    this.setState({ showMessage: true });
 
     console.log(this.state.barcode);
     return axios
@@ -24,84 +27,89 @@ class Scan extends Component {
         barcode: this.state.barcode
       })
       .then(resp => {
-        console.log(resp)
+        this.setState({ showResponse: resp.data.data });
+        this.setState({ showText: true });
       })
       .catch(error => {
         console.log("error", error);
-      })
+      });
   };
 
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
-    console.log(this.state);
   };
 
+  /*
   handleSubmit = event => {
-    const { barcode } = this.state;
     event.preventDefault();
+    const { barcode } = this.state;
     console.log(barcode);
     console.log("Barcode Entered");
-  };
+  };*/
 
   callAdd = event => {
     event.preventDefault();
-    this.setState({showAdd: true});
-    this.setState({showDelete: false});
-    this.setState({showDetails: false});
-    console.log("Current State: ", this.state);
+    this.setState({ checked: event.target.name });
+    /*
+    this.setState({ showDelete: false });
+    this.setState({ showDetails: false });
+    */
   };
+  /*
   callDelete = event => {
     event.preventDefault();
-    this.setState({showAdd: false});
-    this.setState({showDelete: true});
-    this.setState({showDetails: false});
+    this.setState({ showAdd: false });
+    this.setState({ showDelete: true });
+    this.setState({ showDetails: false });
     console.log("Current State: ", this.state);
   };
   callDetails = event => {
     event.preventDefault();
-    this.setState({showAdd: false});
-    this.setState({showDelete: false});
-    this.setState({showDetails: true});
+    this.setState({ showAdd: false });
+    this.setState({ showDelete: false });
+    this.setState({ showDetails: true });
     console.log("Current State: ", this.state);
-  };
-
+  };*/
 
   render() {
-    const { error, barcode } = this.state;
+    const { barcode } = this.state;
     const isInvalid = barcode === "";
     return (
       <div className="scan-wrapper">
-        <form onSubmit={this.handleSubmit}>
-          <div className="scan-container">
-            <h1>Scan An Item</h1>
+        <form onSubmit={this.onButtonClickHandler}>
+          <div className="container">
+            <h1 className="text-center">Scan An Item</h1>
 
-            <button
-              className="button add-button"
-              type="add"
-              onClick={this.callAdd}
-            >
-              Add
-            </button>
+            <div classname="form-buttons text-center">
+              <button
+                className="btn btn-primary add-button text-center"
+                name="add-button"
+                checked={this.state.checked === "add-button"}
+                onClick={this.callAdd}
+              >
+                Add
+              </button>
 
-            <button
-              className="button delete-button"
-              type="delete"
-              onClick={this.callDelete}
-            >
-              Delete
-            </button>
+              <button
+                className="btn  btn-primary delete-button text-center"
+                name="delete-button"
+                checked={this.state.checked === "delete-button"}
+                onClick={this.callAdd}
+              >
+                Delete
+              </button>
 
-            <button
-              className="button details-button"
-              type="details"
-              onClick={this.callDetails}
-            >
-              Details
-            </button>
-
-
+              <button
+                className="btn btn-primary details-button text-center"
+                name="seeDetails-button"
+                checked={this.state.checked === "seeDetails-button"}
+                onClick={this.callAdd}
+              >
+                Details
+              </button>
+            </div>
           </div>
 
           <input
@@ -112,26 +120,33 @@ class Scan extends Component {
             className="scan-input"
           />
 
-          <div>
-            {this.state.showMessage && <p>Hi</p>}
-          </div>
-
-
-
-          <button
-            className="scan-submit-button"
-            onClick={this.onButtonClickHandler}
-          >
-            Find Barcode
-          </button>
+          <button className="scan-submit-button">Find Barcode</button>
         </form>
+        {this.state.showText ? (
+          <ul>
+            <li>
+              <b>Name: </b> {this.state.showResponse.name}
+            </li>
+            <li>
+              <b>Count: </b>
+              {this.state.showResponse.count}
+            </li>
+            <li>
+              <b>ID:</b>
+              {this.state.showResponse.id}
+            </li>
+            <li>
+              <b>barcode: </b>
+              {this.state.showResponse.barcode}
+            </li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 export default Scan;
-
 
 //class App extends Component{
 //  onButtonClickHandler = () => {
@@ -146,6 +161,5 @@ export default Scan;
 //
 //  }
 //}
-
 
 // Scan container page
